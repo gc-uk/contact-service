c0d2a1f8a5940a07799f7a8971c1a191a7bdab0f
Cian Scollan <cscollan@gamblingcommission.gov.uk>
2019-11-20 15:56:39 +0000

    routing

--- routes/routes.js
+++ routes/routes.js
@@ -36,10 +36,22 @@ router.get('/form/complete', formController.form_complete_get);
 // Repeat this for the forms
 router.get('/form/gen/info-details', formController.info_details_get);
 router.post('/form/gen/info-details', formController.info_details_post);
-// router.get('/form/gen/info-contact', formController.info_contact_get);
 
 router.get('/form/gen/info-contact', formController.info_contact_get);
 router.post('/form/gen/info-contact', formController.info_contact_post);
+
+router.get('/form/adr', formController.adr_get);
+router.post('/form/adr', formController.adr_post);
+router.get('/form/gen', formController.gen_get);
+router.post('/form/gen', formController.gen_post);
+router.get('/form/se', formController.se_get);
+router.post('/form/se', formController.se_post);
+router.get('/form/sg', formController.sg_get);
+router.post('/form/sg', formController.sg_post);
+router.get('/form/sr', formController.sr_get);
+router.post('/form/sr', formController.sr_post);
+router.get('/form/w', formController.w_get);
+router.post('/form/w', formController.w_post);
 
 router.get('/form/:id', formController.form_ID_get);
 
